34cf5accbea3b1677b2ec819839362754227cc75
gimnayoung <kny_7_@naver.com>
2023-07-05 23:32:56 +0900

    seb

--- src/App.js
+++ src/App.js
@@ -1,53 +1,57 @@
+import { useEffect, useState } from "react";
+import { Route, Routes, Link, useNavigate, Outlet } from "react-router-dom";
+import "./App.css";
 
-import { useEffect, useState } from 'react';
-import { Route,Routes,Link,useNavigate,Outlet } from 'react-router-dom'
-import './App.css';
-
-import Nav from './components/nav'
-import Data from'./data/data.json'
-import List from './components/list';
-import MainBG from './components/mainBG';
-import Detail from './components/detail';
-import Event from './components/event';
-import Footer from './components/footer';
-import Boom from './components/boom';
+import Nav from "./components/nav";
+import Data from "./data/data.json";
+import List from "./components/list";
+import MainBG from "./components/mainBG";
+import Detail from "./components/detail";
+import Event from "./components/event";
+import Footer from "./components/footer";
+import Boom from "./components/boom";
 
 function App() {
   // console.log(shoes);
-  let navigate=useNavigate();
-  const [shoes]=useState(Data);
+  let navigate = useNavigate();
+  const [shoes] = useState(Data);
 
   return (
     <div>
       <Routes>
-        <Route path='/' element={
-          <>
-          <Boom/>
-            <div>
-              <Nav/>
-            </div>
-            <MainBG/>
-            <h1>오늘은 이 상품 어때요?</h1>
-            <div>
-              <List />
-            </div>
-            <button>더보기</button>
-        </>
-        }>
+        <Route
+          path="/"
+          element={
+            <>
+              {/* <Boom/> */}
+              <div>
+                <Nav />
+              </div>
+              <MainBG />
+              <h1>오늘은 이 상품 어때요?</h1>
+              <div>
+                <List />
+              </div>
+              <button>더보기</button>
+            </>
+          }
+        ></Route>
+        <Route path="/detail">
+          <Route path="0" element={<Detail id={0} />}></Route>
+          <Route path="1" element={<Detail id={1} />}></Route>
+          <Route path="2" element={<Detail id={2} />}></Route>
         </Route>
-        <Route path="/detail" >
-          <Route path='0' element={<Detail id={0}/>}></Route>
-          <Route path='1' element={<Detail id={1} />}></Route>
-          <Route path='2' element={<Detail id={2} />}></Route>
-        </Route>
-        <Route path='/login' element={<div>로그인페이지</div>}></Route>
-        <Route path='*' element={<div>에러페이지</div>}></Route>
-        <Route path='/event'  element={<Event/>}>
-          <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
-          <Route path='two' element={<div>생일기념 쿠폰받기</div>}></Route>
+        <Route path="/login" element={<div>로그인페이지</div>}></Route>
+        <Route path="*" element={<div>에러페이지</div>}></Route>
+        <Route path="/event" element={<Event />}>
+          <Route
+            path="one"
+            element={<div>첫 주문시 양배추즙 서비스</div>}
+          ></Route>
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>}></Route>
         </Route>
         {/* //test */}
-        <Route path='/test' element={<Boom/>}></Route>
+        <Route path="/test" element={<Boom />}></Route>
       </Routes>
       {/* <div>
         <Nav/>
--- src/components/mainBG.js
+++ src/components/mainBG.js
@@ -1,10 +1,51 @@
-function MainBG(){
-    return(
-        <div>
-            <div className='main-bg'>
-            </div>
+import Main1 from "../img/main1.jpg";
+import Main2 from "../img/main2.jpg";
+import Main3 from "../img/main3.jpg";
+import Main4 from "../img/main4.jpg";
+
+function MainBG() {
+  return (
+    <div>
+      <div className="mainBg">
+        <input type="radio" name="slide" id="slide01" checked />
+        <input type="radio" name="slide" id="slide02" />
+        <input type="radio" name="slide" id="slide03" />
+        <input type="radio" name="slide" id="slide04" />
+        <div className="slidewrap">
+          <ul className="mainBg-ul">
+            <li className="mainBg-li">
+              <a>
+                <label for="slide04" className="leftslide"></label>
+                <img src={Main1} />
+                <label for="slide02" className="rightslide"></label>
+              </a>
+            </li>
+            <li className="mainBg-li">
+              <a>
+                <label for="slide01" className="leftslide"></label>
+                <img src={Main2} />
+                <label for="slide03" className="rightslide"></label>
+              </a>
+            </li>
+            <li className="mainBg-li">
+              <a>
+                <label for="slide02" className="leftslide"></label>
+                <img src={Main3} />
+                <label for="slide04" className="rightslide"></label>
+              </a>
+            </li>
+            <li className="mainBg-li">
+              <a>
+                <label for="slide03" className="leftslide"></label>
+                <img src={Main4} />
+                <label for="slide01" className="rightslide"></label>
+              </a>
+            </li>
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default MainBG;
