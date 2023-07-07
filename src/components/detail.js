ae5870ea6da32106a8e9c7047745b0faed9a4a2d
nayoung <kny_7_@naver.com>
2023-07-07 11:58:46 +0900

    UI만 만듬

--- src/components/detail.js
+++ src/components/detail.js
@@ -3,9 +3,20 @@ import { useEffect, useState } from "react";
 
 function Detail(props) {
   const [alert, setAlert] = useState(true);
+  const [tap,setTap]=useState(0);
+  const [color,setColor] =useState('')
+
+  const clolrHandling=(e)=>{
+    setColor(()=>{
+      return e.target.value;
+    })
+  }
+  // useEffect(()=>{
+  //   setColor('click')
+  // },[tap])
   // const [count, setCount] = useState(0);
   let { id } = useParams();
-  //detail 처음 장착됐을때,업데이트될때 실행
+  // detail 처음 장착됐을때,업데이트될때 실행
   // useEffect(() => {
   //   setTimeout(() => {
   //     setAlert(false);
@@ -28,8 +39,53 @@ function Detail(props) {
           <button className="button">주문하기</button>
         </div>
       </div>
+      <div className="detail-tap">
+        <div>
+          <form>
+            <div className="detailbutton">
+              <button value={0} type="button" onClick={()=>{setTap(0);}}>
+                <label for="menu-1"> 상품정보
+                  <input type="radio" id="menu-1" name="tap" checked/>
+                </label>
+              </button>
+            </div>
+            <div className="detailbutton">
+              <button value={1} type="button" onClick={()=>{setTap(1);}}>
+                <label  for="menu-2"> 리뷰
+                  <input type="radio" id="menu-2" name="tap"/>
+                </label>
+              </button>
+            </div>
+            <div className="detailbutton">
+              <button value={2} type="button" onClick={()=>{setTap(2);}}>
+                <label  for="menu-3">Q&A
+                  <input type="radio" id="menu-3" name="tap"/>
+                </label>  
+              </button>
+            </div>
+            <div className="detailbutton"> 
+              <button value={3} type="button" onClick={()=>{setTap(3);}}>
+                <label  for="menu-4">주문정보
+                  <input type="radio" id="menu-4" name="tap"/>
+                </label>  
+              </button>
+            </div>
+          </form>
+          <div>
+            <TapCont tap={tap}></TapCont>
+          </div>
+        </div>
+      </div> 
     </div>
   );
 }
 
+function TapCont({tap}){
+  return[
+   <div>상품정보 입니다</div>,
+   <div>리뷰 입니다</div>,
+   <div>문의 입니다</div>,
+   <div>주문정보 입니다</div>
+  ][tap]
+}
 export default Detail;
