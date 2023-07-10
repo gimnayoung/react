dd5f7ae05edb74be1d43ac14cbfb182f832954ed
nayoung <kny_7_@naver.com>
2023-07-10 16:03:31 +0900

    test

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,24 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+//detial 페이지에서 주문하기 버튼을 누르면 추가되는ㄴ
 let cartlist = createSlice({
   name: "cartlist",
   initialState: [
-    { id: 0, name: "white and black", count: 2 },
-    { id: 2, name: "Gray Yordan", count: 1 },
+    { id:0,title:"",img:"",count:0,price:""}
   ],
   reducers: {
-    changeCount(state) {
-      return state.count + 1;
+    changeCount(state) { return state.count + 1;
     },
+    addItem(state,action){
+       state.push(action.payload)
+    }
   },
 });
 // export let { changeCount } = cartlist.action
-
 export default configureStore({
   reducer: {
     cartlist: cartlist.reducer,
   },
 });
+
+export let {addItem} =cartlist.actions
