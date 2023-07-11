1e50c2c1612af69472d8cac043a9267be4be252b
nayoung <kny_7_@naver.com>
2023-07-11 10:43:32 +0900

    최근본상품구현

--- src/components/detail.js
+++ src/components/detail.js
@@ -11,6 +11,37 @@ import { PiBasketLight } from "react-icons/pi";
 import { FiHeart } from "react-icons/fi";
 
 function Detail(props) {
+  //최근 본 상품
+  useEffect(()=>{
+    watchedUser();
+  },[])
+  const watchedUser=function(){
+    var myArr = localStorage.getItem('watchedUser');
+    if(myArr===null){
+      myArr =[];
+    }else{
+      myArr=JSON.parse(myArr);
+    }
+    myArr.push(
+      {"id":찾은상품.id,
+      "title":찾은상품.title,
+      "img":찾은상품.img,
+      "price":찾은상품.price},
+    )
+    //중복방지 코드
+    // myArr=new Set(myArr);
+    // myArr = [...myArr];
+    const filteredArr = myArr.reduce((acc, current) => {
+      const x = acc.find(item => item.id === current.id);
+      if (!x) {
+        return acc.concat([current]);
+      } else {
+        return acc;
+      }
+    }, []);
+    localStorage.setItem('watchedUser',JSON.stringify(filteredArr))
+    // console.log(myArr);
+  }
   // window.localStorage.setItem("title", JSON.stringify([uselocalStorage]));
   // useEffect(()=>{
   //   let 꺼낸거 = localStorage.getItem("id")
@@ -38,7 +69,7 @@ function Detail(props) {
   //   꺼냄.push(찾은상품.title)
   //   localStorage.setItem('title',JSON.stringify(꺼냄))
   // })
-  const localHandling = function () {
+  // const localHandling = function () {
     // let 꺼내= sessionStorage.getItem("title")
     // setLocalStorage([...uselocalStorage,props.shoes[id].title])
     // console.log(uselocalStorage) //['빈티지']
@@ -46,16 +77,16 @@ function Detail(props) {
     // let 다시꺼내 = sessionStorage.getItem("title")
     // console.log(다시꺼내)
     // dispatch(addItem( { id:props.shoes[id].id,title:props.shoes[id].title,img:"",count:1,price:props.shoes[id].price}))
-    let 꺼냄 = localStorage.getItem("title", JSON.stringify([찾은상품.title])); //안녕
+    // let 꺼냄 = localStorage.getItem("title", JSON.stringify([찾은상품.title])); //안녕
     // 꺼냄 = JSON.stringify(꺼냄);
-    console.log(꺼냄);
-    setLocalStorage([...uselocalStorage, 꺼냄]);
-    console.log(uselocalStorage);
+    // console.log(꺼냄);
+    // setLocalStorage([...uselocalStorage, 꺼냄]);
+    // console.log(uselocalStorage);
     // setLocalStorage([...uselocalStorage,...꺼냄])
     // 꺼냄.push(찾은상품.title);
     // localStorage.setItem("title", JSON.stringify(꺼냄));
     // console.log(uselocalStorage);
-  };
+  // };
   const [alert, setAlert] = useState(true);
   const [tap, setTap] = useState(0);
   const [color, setColor] = useState("");
@@ -115,7 +146,7 @@ function Detail(props) {
             <a
               className="button-submitA"
               onClick={() => {
-                localHandling();
+                // localHandling();
                 // localStoragse.setItem("title",JSON.stringify([props.shoes[id].title]))
               }}
             >
