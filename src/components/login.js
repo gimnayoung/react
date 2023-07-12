3dcf9576a81eaf01f7b382811e266d6764962f43
nayoung <kny_7_@naver.com>
2023-07-12 13:27:54 +0900

    제출:07.12

--- src/components/login.js
+++ src/components/login.js
@@ -7,15 +7,15 @@ function Login(){
                 <input type='email' placeholder='이메일 또는 전화번호'/>
                 <input type='password' placeholder='비밀번호'/>
                 <label for='remember-check'>
-                    <input type='checkbox' id='remember-check'/>자동 로그인
+                    <input type='checkbox' id='remember-check'/>자동   로그인
                 </label>
                 <input type="submit" value='로그인' class="loginBut"/>
                 <div class="forgetBox">
-                    <a class="forgetID" href='#'>아이디찾기</a>
+                    <a class="forgetID" href='#'>아이디  찾기</a>
                     <a href='#'>비밀번호찾기</a>
                 </div>
                 <div class="signupBox">
-                    <button class="signupBut">새 계정 만들기</button>
+                    <button class="signupBut">새 계정만들기</button>
                 </div>
             </form>
         </div>
